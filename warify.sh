1bdc87018bd7d46b6d61a6f89c7427dcf1ec17d8
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-02-13 18:12:29 +0100

    Includes the service name in the WAR

--- warify.sh
+++ warify.sh
@@ -6,6 +6,7 @@ rm -fR /app/target
 mkdir -p /app/target/app/ext
 cp -R /warification/{app,lib} /app/target/
 cp -R /opt/mu-ruby-template-${MU_RUBY_TEMPLATE_VERSION#v*}/{lib,sinatra_template,web.rb} /app/target/app/
+echo ${SERVICE_NAME?} >> /app/target/app/service_name
 cp -R `find * -maxdepth 0 -not -name target` /app/target/app/ext/
 
 cp -R /warification/{Gemfile,config.ru,config} /app/target/
